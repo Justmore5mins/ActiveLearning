27d17b037a25497f54615746b5bb2aa85b654a56
Justmore5mins <justmore5mins@gmail.com>
2025-01-15 06:31:48 +0800

    functions the imagedetect.py

--- ImageDetect.py
+++ ImageDetect.py
@@ -1,8 +1,10 @@
 from os import listdir
 from os.path import isdir
-from cv2 import VideoCapture, imread
+from cv2 import VideoCapture, imread,CAP_PROP_FRAME_HEIGHT,CAP_PROP_FRAME_WIDTH
+from sympy import true
 from ultralytics import YOLO
 from PIL import Image
+from ImageCollect import ImageCollect
 
 class ImageDetect:
     def __init__(self, model:str="yolo11n.pt",source:str|int = 0,device:int|str|list[int] = "cpu"):
@@ -14,6 +16,7 @@ class ImageDetect:
             self.source = source
             
         self.model.to(device) if device != "cpu" else None
+        self.collect = ImageCollect("detected")
         
     def static(self,save:bool = False):
         if isdir(self.source):
@@ -36,14 +39,28 @@ class ImageDetect:
                     with open(f"{item.split(".")[0]}.txt","w") as file:
                         file.writelines(infos)
                         
-    def stream(self,conf:float = 0.8, cls:list[str] = None):
-        if not (0 <= conf <= 1):
+    def stream(self,conf:float = 0.8, cls:list[str] = None,threshold:float = 0.9,interval:int = 5):
+        if not (0 <= conf <= 1) or (0 <= threshold <= 1):
             raise ValueError("Confidence must be in 0 and 1.")
-        
         if not cls:
             cls = self.model.names
-            
         
+        state:bool = True    
+        while state:
+            state, image = self.cam.read()
+            result = self.model.predict(image,True)
+            items = []
+            for res in result:
+                if not res.boxes:
+                    continue
+                if max(res.boxes.conf.tolist()) >= threshold:
+                    xywh = res.boxes.xywh
+                    for item in zip(res.boxes.cls.tolist(), xywh[0].tolist(), xywh[1].tolist(), xywh[2].tolist(), xywh[3].tolist()):
+                        items.apped(f"{item[0]} {item[1]/CAP_PROP_FRAME_WIDTH} {item[2]/CAP_PROP_FRAME_HEIGHT} {item[3]/CAP_PROP_FRAME_WIDTH} {item[4]/CAP_PROP_FRAME_HEIGHT}")
+                    self.collect.save(image,True,items)
+                else:
+                    self.collect.save(image)
+                    
         
     def __CamInit__(self,source:int) -> VideoCapture:
         return VideoCapture(source)
